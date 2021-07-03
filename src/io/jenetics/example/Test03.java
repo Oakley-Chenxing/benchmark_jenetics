74eee98e264aa1a0b1a482b20e3512a305961b4f
Qakley-Chenxing <Chenxing.li@northumbria.ac.uk>
2021-07-03 23:18:41 +0800

    Update at 03/07/21

---
+++ src/io/jenetics/example/Test03.java
@@ -0,0 +1,48 @@
+package io.jenetics.example;
+
+import io.jenetics.Optimize;
+import io.jenetics.Phenotype;
+import io.jenetics.engine.Codec;
+import io.jenetics.DoubleGene;
+import io.jenetics.engine.Codecs;
+import io.jenetics.engine.Engine;
+import io.jenetics.engine.EvolutionStatistics;
+import io.jenetics.util.DoubleRange;
+
+import static io.jenetics.engine.EvolutionResult.toBestPhenotype;
+
+import io.jenetics.Chromosome;
+
+public class Test03 {
+    public static void main(String[] args){
+        solve();
+    }
+    public static double evaluate(double[] positions){
+        double sum=0;
+        for(double i:positions){
+            sum+=i*i;
+        }
+        return sum/positions.length;
+    }
+    public static void solve(){
+        final Engine<DoubleGene,Double> engine= Engine.builder(Test03::evaluate,
+                Codecs.ofVector(DoubleRange.of(-100,100),20))
+                .optimize(Optimize.MINIMUM).build();
+        final EvolutionStatistics<Double, ?>
+                statistics = EvolutionStatistics.ofNumber();
+
+        final Phenotype<DoubleGene, Double> best = engine.stream()
+                .limit(3000)
+                .peek(statistics)
+                .collect(toBestPhenotype());
+
+        Object[] bestPosition = best.genotype().chromosome().toSeq().asList().toArray();;
+        double result=best.fitness();
+        for (int i = 0; i < bestPosition.length; i++) {
+            DoubleGene dg = (DoubleGene) bestPosition[i];
+            System.out.print(dg.allele() + "\t");
+        }
+        System.out.println("\n"+result);
+
+    }
+}
